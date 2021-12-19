ae9a8eb8d995c3c371f3282bd124becc79df5e71
sakshi uppoor <sakshiuppoor@gmail.com>
2021-12-19 20:45:05 +0530

    Add limit parameter to list API

--- videosApp/views.py
+++ videosApp/views.py
@@ -2,13 +2,13 @@ from django.shortcuts import render
 from rest_framework.generics import ListAPIView
 from .models import Video
 from .serializers import VideoSerializer
-from rest_framework.pagination import PageNumberPagination
+from .utils.custom_paginator import CustomPageNumberPagination
 # Create your views here.
 
 class VideoView(ListAPIView):
     model = Video
     serializer_class = VideoSerializer
-    pagination_class = PageNumberPagination 
+    pagination_class = CustomPageNumberPagination 
     queryset = Video.objects.all().order_by('-published_at')
 
 def dashboard(request):
