9d311ea849538a75d4cfe04d6f9dc94a4a3fde47
sakshi uppoor <sakshiuppoor@gmail.com>
2021-12-19 16:47:56 +0530

    Add util function to save videos to db

--- videosApp/utils/youtube_api.py
+++ videosApp/utils/youtube_api.py
@@ -1,5 +1,7 @@
 from googleapiclient.discovery import build
+from django.db import IntegrityError
 from datetime import datetime, timedelta
+from ..models import Video
 import json
 
 # Fetching youtube API Key
@@ -11,7 +13,8 @@ youtube = build('youtube', 'v3', developerKey=YOUTUBE_API_KEY)
 
 def fetchLatestVideos():
     """
-    Fetch the latest 10 chess videos published an hour ago
+    Fetches and stores the latest 10 
+    chess videos published an hour ago
     """
     
     # Timestamp of 1 hour before
@@ -25,5 +28,31 @@ def fetchLatestVideos():
         maxResults=10, # Fetch max 10 videos at a time
         publishedAfter=an_hour_ago # Fetch videos posted not more than an hour ago
     )
+
     response = request.execute()
-    return response
+    videos = storeVideos(response["items"])
+    return {"videos":videos}
+
+def storeVideos(videos):
+    saved_videos = []
+    for video in videos:
+        video_dict = {
+            "id":video["id"]["videoId"],
+            "title":video["snippet"]["title"],
+            "description":video["snippet"]["description"],
+            "thumbnail_url":video["snippet"]["thumbnails"]["default"]["url"],
+            "channel_name":video["snippet"]["channelTitle"],
+            "published_at":video["snippet"]["publishedAt"],
+        }
+        try:
+            Video.objects.create(**video_dict)
+            saved_videos.append(video_dict)
+        except IntegrityError: 
+            """
+            If video with same id is already saved in db,
+            then the same video got fetched again
+            To avoid duplication, we don't want to save this.
+            Hence, the Integrity exception is used
+            """
+            pass
+    return saved_videos
