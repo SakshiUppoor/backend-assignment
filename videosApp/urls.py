13e22a3ee58cefed174d5cb55e9f4133bafbf652
sakshi uppoor <sakshiuppoor@gmail.com>
2021-12-19 21:19:37 +0530

    Update API endpoint URL

--- videosApp/urls.py
+++ videosApp/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.VideoView.as_view()),
+    path('api/list',views.VideoView.as_view()),
     path('dashboard',views.dashboard),
 ]
