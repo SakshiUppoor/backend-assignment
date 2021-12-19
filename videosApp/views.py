c50c2cfc4dd2994d3964181f81d1c525b50df91c
sakshi uppoor <sakshiuppoor@gmail.com>
2021-12-19 18:18:28 +0530

    Create GET videos API endpoint

--- videosApp/views.py
+++ videosApp/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
-
+from rest_framework.generics import ListAPIView
+from .models import Video
+from .serializers import VideoSerializer
+from rest_framework.pagination import PageNumberPagination
 # Create your views here.
+
+class VideoView(ListAPIView):
+    model = Video
+    serializer_class = VideoSerializer
+    pagination_class = PageNumberPagination 
+    queryset = Video.objects.all().order_by('-published_at')
